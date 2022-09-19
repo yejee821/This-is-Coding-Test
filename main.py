fb815c787c38ccf2f519979b8ab5dacbf1c3f80b
Lee Yejee <73184269+yejee821@users.noreply.github.com>
2022-09-19 15:58:22 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+# 샘플 Python 스크립트입니다.
+
+# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
+# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+
+
+def print_hi(name):
+    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
+    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+
+
+# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
+if __name__ == '__main__':
+
+    # N은 세로
+    # M은 가로
+    NM=input()
+    NM=list(map(int,NM.split()))
+    N=NM[0]
+    M=NM[1]
+
+    # 카드는 총 N*M개의 숫자가 있다.
+
+    num=[]
+
+    for i in range(N):
+        card=input()
+        card=list(map(int,card.split()))
+        num.append(card)
+
+    # print(num)
+
+    big=0
+
+    for i in range(N):
+        if big<min(num[i]):
+            big=min(num[i])
+        else:
+            pass
+
+    print(big)
+
+# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
